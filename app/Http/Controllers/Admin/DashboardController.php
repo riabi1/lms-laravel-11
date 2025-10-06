e238969908fa3425fab76959b87c2197c43ec467
riabi1 <riabimedali18@gmail.com>
2025-10-06 16:26:45 +0100

    feat: integrate admin frontend

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    function index(): View
+    {
+        return view('admin.dashboard');
+    }
+}
